232428272ed27bc0b71b873cc0e9ea921e62b814
syeomans <samuelyeomans@gmail.com>
2016-10-24 23:00:49 -0400

    Add files via upload

---
+++ Switch_led.ino
@@ -0,0 +1,49 @@
+//Sets input and output pins
+#define In1 13
+#define In2 12
+#define In3 11
+#define In4 10
+#define Out1 9
+#define Out2 8
+#define Out3 7
+#define Out4 6
+
+void setup()
+{
+//Initializes pins 1-4 as inputs and 5-8 as outputs
+  pinMode(In1,INPUT);
+  pinMode(In2,INPUT);
+  pinMode(In3,INPUT);
+  pinMode(In4,INPUT);
+  pinMode(Out1,OUTPUT);
+  pinMode(Out2,OUTPUT);
+  pinMode(Out3,OUTPUT);
+  pinMode(Out4,OUTPUT);
+}
+
+void loop()
+{
+  //In1 drives Out1
+  if (digitalRead(In1))
+  {digitalWrite(Out1,HIGH);}
+  else
+  {digitalWrite(Out1,LOW);}
+
+  //In2 drives Out2
+  if (digitalRead(In2) == 1)
+  {digitalWrite(Out2,HIGH);}
+  else
+  {digitalWrite(Out2,LOW);}
+
+  //In3 drives Out3
+  if (digitalRead(In3) == 1)
+  {digitalWrite(Out3,HIGH);}
+  else
+  {digitalWrite(Out3,LOW);}
+
+  //In4 drives Out4
+  if (digitalRead(In4) == 1)
+  {digitalWrite(Out4,HIGH);}
+  else
+  {digitalWrite(Out4,LOW);}
+}
